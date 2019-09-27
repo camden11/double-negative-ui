4b65f9837f95a31441d9262d2b09c384ffe8ff84
Camden Phalen <camdenphalen@gmail.com>
2019-09-27 16:29:53 -0400

    support multiple authors

--- components/postPreview.js
+++ components/postPreview.js
@@ -4,93 +4,100 @@ import Moment from "react-moment";
 import _ from "lodash";
 import Heading from "./heading";
 
-const PostPreview = ({ post }) => (
-  <>
-    <Link
-      href={{ pathname: "/post/[slug]", query: { slug: post.slug } }}
-      as={`/post/${post.slug}`}
-    >
-      <a className="post-preview">
-        <span className="post-category">
-          {_.get(post, "categories[0].name")}
-        </span>
-        <div
-          className="image"
-          style={{ backgroundImage: `url(${_.get(post, "featureImage.url")})` }}
-        />
-        <div className="post-info">
-          <div className="post-heading">
-            <Heading level={3}>{post.title}</Heading>
+const PostPreview = ({ post }) => {
+  const byline = post.author
+    ? `by ${_.get(post, "author.name")}`
+    : "Multiple contributors";
+  return (
+    <>
+      <Link
+        href={{ pathname: "/post/[slug]", query: { slug: post.slug } }}
+        as={`/post/${post.slug}`}
+      >
+        <a className="post-preview">
+          <span className="post-category">
+            {_.get(post, "categories[0].name")}
+          </span>
+          <div
+            className="image"
+            style={{
+              backgroundImage: `url(${_.get(post, "featureImage.url")})`
+            }}
+          />
+          <div className="post-info">
+            <div className="post-heading">
+              <Heading level={3}>{post.title}</Heading>
+            </div>
+            <div className="post-meta">
+              <span className="post-byline">{byline}</span>
+              <span className="post-date">
+                {" /"} <Moment date={post.publishDate} format="MMM DD YYYY" />
+              </span>
+            </div>
           </div>
-          <div className="post-meta">
-            <span className="post-byline">by {_.get(post, "author.name")}</span>
-            <span className="post-date">
-              {" /"} <Moment date={post.publishDate} format="MMM DD YYYY" />
-            </span>
-          </div>
-        </div>
-      </a>
-    </Link>
-    <style jsx>{`
-      .post-preview {
-        display: flex;
-        flex-direction: column;
-        border: 2px solid #000;
-        grid-column: span 4;
-        color: #000;
-        text-decoration: none;
-        position: relative;
-        margin-bottom: 60px;
-         {
-          /* overflow: visible; */
-        }
-      }
-
-      .post-preview:visited {
-        color: #000;
-      }
-
-      @media (max-width: 992px) {
+        </a>
+      </Link>
+      <style jsx>{`
         .post-preview {
-          grid-column: span 6;
+          display: flex;
+          flex-direction: column;
+          border: 2px solid #000;
+          grid-column: span 4;
+          color: #000;
+          text-decoration: none;
+          position: relative;
+          margin-bottom: 60px;
+           {
+            /* overflow: visible; */
+          }
         }
-      }
 
-      @media (max-width: 768px) {
-        .post-preview {
-          grid-column: span 12;
+        .post-preview:visited {
+          color: #000;
         }
-      }
 
-      .post-category {
-        display: block;
-        position: absolute;
-        transform: rotate(-90deg);
-        transform-origin: bottom right;
-        text-transform: uppercase;
-        right: calc(100% + 6px);
-        top: -20px;
-      }
+        @media (max-width: 992px) {
+          .post-preview {
+            grid-column: span 6;
+          }
+        }
 
-      .image {
-        width: 100%;
-        padding-bottom: 66.66666%;
-        background-size: cover;
-        background-position: center center;
-      }
+        @media (max-width: 768px) {
+          .post-preview {
+            grid-column: span 12;
+          }
+        }
 
-      .post-info {
-        padding: 20px 15px;
-        display: flex;
-        flex-direction: column;
-        flex-grow: 1;
-      }
+        .post-category {
+          display: block;
+          position: absolute;
+          transform: rotate(-90deg);
+          transform-origin: bottom right;
+          text-transform: uppercase;
+          right: calc(100% + 6px);
+          top: -20px;
+        }
 
-      .post-heading {
-        flex-grow: 1;
-      }
-    `}</style>
-  </>
-);
+        .image {
+          width: 100%;
+          padding-bottom: 66.66666%;
+          background-size: cover;
+          background-position: center center;
+        }
+
+        .post-info {
+          padding: 20px 15px;
+          display: flex;
+          flex-direction: column;
+          flex-grow: 1;
+        }
+
+        .post-heading {
+          flex-grow: 1;
+        }
+      `}</style>
+    </>
+  );
+};
 
 export default PostPreview;
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -8,189 +8,194 @@ import Gallery from "../../components/gallery";
 import Layout from "../../layouts/base";
 import Strapi from "../../transport/strapi";
 
-const Post = ({ post }) => (
-  <>
-    <Layout>
-      <div className="post-grid">
-        <div className="post-heading-column">
-          <Heading level={1}>{post.title}</Heading>
-          <p className="subheading">{post.blurb}</p>
-        </div>
-        <div className="post-meta-column">
-          <span className="post-byline">by {_.get(post, "author.name")}</span>
-          <br />
-          <span className="post-date">
-            <Moment date={post.publishDate} format="MMM DD YYYY" />
-          </span>
-          <ul className="post-categories">
-            {_.get(post, "categories", []).map((category, index) => (
-              <li key={index}>
-                <Link href={`/?category=${category.slug}`}>
-                  <a>{category.name}</a>
-                </Link>
-              </li>
-            ))}
-            {_.get(post, "genres", []).map((genre, index) => (
-              <li key={index}>
-                <Link href={`/?genre=${genre.slug}`}>
-                  <a>{genre.name}</a>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="post-content-column">
-          <img
-            className="post-feature-image"
-            src={_.get(post, "featureImage.url")}
-          />
-          <div className="post-mobile-meta">
-            <span className="post-byline">by {_.get(post, "author.name")}</span>
+const Post = ({ post }) => {
+  const byline = post.author
+    ? `by ${_.get(post, "author.name")}`
+    : "Multiple contributors";
+  return (
+    <>
+      <Layout>
+        <div className="post-grid">
+          <div className="post-heading-column">
+            <Heading level={1}>{post.title}</Heading>
+            <p className="subheading">{post.blurb}</p>
+          </div>
+          <div className="post-meta-column">
+            <span className="post-byline">${byline}</span>
             <br />
             <span className="post-date">
               <Moment date={post.publishDate} format="MMM DD YYYY" />
             </span>
-          </div>
-          <Markdown content={post.content} />
-          <div className="post-mobile-footer-meta">
-            <p>
-              Tagged under:{" "}
-              {post.categories.map((category, index) => (
-                <span key={index}>
+            <ul className="post-categories">
+              {_.get(post, "categories", []).map((category, index) => (
+                <li key={index}>
                   <Link href={`/?category=${category.slug}`}>
-                    <a className="post-mobile-meta-item">{category.name}</a>
-                  </Link>{" "}
-                </span>
+                    <a>{category.name}</a>
+                  </Link>
+                </li>
               ))}
-              {post.genres.map((genre, index) => (
-                <span key={index}>
+              {_.get(post, "genres", []).map((genre, index) => (
+                <li key={index}>
                   <Link href={`/?genre=${genre.slug}`}>
-                    <a className="post-mobile-meta-item">{genre.name}</a>
-                  </Link>{" "}
-                </span>
+                    <a>{genre.name}</a>
+                  </Link>
+                </li>
               ))}
-            </p>
+            </ul>
           </div>
-          {post.spotifyEmbed && (
-            <div className="spotify">
-              <iframe
-                src={post.spotifyEmbed}
-                width="100%"
-                height="500"
-                frameborder="0"
-                allowtransparency="true"
-                allow="encrypted-media"
-              ></iframe>
+          <div className="post-content-column">
+            <img
+              className="post-feature-image"
+              src={_.get(post, "featureImage.url")}
+            />
+            <div className="post-mobile-meta">
+              <span className="post-byline">{byline}</span>
+              <br />
+              <span className="post-date">
+                <Moment date={post.publishDate} format="MMM DD YYYY" />
+              </span>
             </div>
-          )}
+            <Markdown content={post.content} />
+            <div className="post-mobile-footer-meta">
+              <p>
+                Tagged under:{" "}
+                {post.categories.map((category, index) => (
+                  <span key={index}>
+                    <Link href={`/?category=${category.slug}`}>
+                      <a className="post-mobile-meta-item">{category.name}</a>
+                    </Link>{" "}
+                  </span>
+                ))}
+                {post.genres.map((genre, index) => (
+                  <span key={index}>
+                    <Link href={`/?genre=${genre.slug}`}>
+                      <a className="post-mobile-meta-item">{genre.name}</a>
+                    </Link>{" "}
+                  </span>
+                ))}
+              </p>
+            </div>
+            {post.spotifyEmbed && (
+              <div className="spotify">
+                <iframe
+                  src={post.spotifyEmbed}
+                  width="100%"
+                  height="500"
+                  frameborder="0"
+                  allowtransparency="true"
+                  allow="encrypted-media"
+                ></iframe>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-      {post.gallery && (
-        <>
-          <h2>Photos</h2>
-          <Gallery gallery={post.gallery} />
-        </>
-      )}
-    </Layout>
-    <style jsx>{`
-      .post-grid {
-        display: grid;
-        grid-template-columns: repeat(12, 1fr);
-        grid-column-gap: 30px;
-      }
-
-      .post-heading-column {
-        grid-row: 1;
-        grid-column: 4 / 11;
-      }
-
-      .post-meta-column {
-        grid-row: 2;
-        grid-column: span 3;
-        border-top: 2px solid #000;
-        padding-top: 20px;
-        margin-right: 20px;
-      }
-
-      .post-content-column {
-        grid-row: 2;
-        grid-column: span 8;
-      }
-
-      .post-mobile-meta {
-        display: none;
-      }
-
-      .post-mobile-footer-meta {
-        display: none;
-      }
-
-      li {
-        text-transform: uppercase;
-      }
-
-      .subheading {
-        font-size: 20px;
-        margin-bottom: 30px;
-        margin-top: 0;
-      }
-
-      .post-feature-image {
-        margin-bottom: 40px;
-      }
-
-      .post-gallery-container {
-        grid-column: 3 / 13;
-      }
-
-      .spotify {
-        margin-top: 40px;
-      }
-
-      @media (max-width: 768px) {
+        {post.gallery && (
+          <>
+            <h2>Photos</h2>
+            <Gallery gallery={post.gallery} />
+          </>
+        )}
+      </Layout>
+      <style jsx>{`
         .post-grid {
-          grid-column-gap: 0;
+          display: grid;
+          grid-template-columns: repeat(12, 1fr);
+          grid-column-gap: 30px;
         }
 
         .post-heading-column {
-          grid-column: span 12;
+          grid-row: 1;
+          grid-column: 4 / 11;
         }
+
         .post-meta-column {
-          display: none;
+          grid-row: 2;
+          grid-column: span 3;
+          border-top: 2px solid #000;
+          padding-top: 20px;
+          margin-right: 20px;
         }
 
         .post-content-column {
-          grid-column: span 12;
+          grid-row: 2;
+          grid-column: span 8;
         }
 
         .post-mobile-meta {
-          display: block;
-          margin-bottom: 10px;
-        }
-
-        .post-feature-image {
-          margin-bottom: 30px;
-        }
-
-        .post-gallery-container {
-          grid-column: span 12;
+          display: none;
         }
 
         .post-mobile-footer-meta {
-          display: block;
-          border-top: 2px solid #000;
-          margin-top: 40px;
-          padding-top: 10px;
+          display: none;
         }
 
-        .post-mobile-meta-item {
+        li {
           text-transform: uppercase;
-          text-decoration: underline;
         }
-      }
-    `}</style>
-  </>
-);
+
+        .subheading {
+          font-size: 20px;
+          margin-bottom: 30px;
+          margin-top: 0;
+        }
+
+        .post-feature-image {
+          margin-bottom: 40px;
+        }
+
+        .post-gallery-container {
+          grid-column: 3 / 13;
+        }
+
+        .spotify {
+          margin-top: 40px;
+        }
+
+        @media (max-width: 768px) {
+          .post-grid {
+            grid-column-gap: 0;
+          }
+
+          .post-heading-column {
+            grid-column: span 12;
+          }
+          .post-meta-column {
+            display: none;
+          }
+
+          .post-content-column {
+            grid-column: span 12;
+          }
+
+          .post-mobile-meta {
+            display: block;
+            margin-bottom: 10px;
+          }
+
+          .post-feature-image {
+            margin-bottom: 30px;
+          }
+
+          .post-gallery-container {
+            grid-column: span 12;
+          }
+
+          .post-mobile-footer-meta {
+            display: block;
+            border-top: 2px solid #000;
+            margin-top: 40px;
+            padding-top: 10px;
+          }
+
+          .post-mobile-meta-item {
+            text-transform: uppercase;
+            text-decoration: underline;
+          }
+        }
+      `}</style>
+    </>
+  );
+};
 
 Post.getInitialProps = async function({ query }) {
   const { slug } = query;
