32187efadce3da913298f644f8186f6e23869644
Camden Phalen <camdenphalen@gmail.com>
2020-02-04 20:48:50 -0500

    shows meta

--- pages/shows.js
+++ pages/shows.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Prismic from "prismic-javascript";
+import Head from "next/head";
 import PrismicClient from "../transport/prismic";
 import AirtableClient from "../transport/airtable";
 import FilterBar from "../components/filterBar";
@@ -10,6 +11,17 @@ import getSavedCity from "../utils/getSavedCity";
 const Shows = ({ shows, cities, city, genres, genreQuery }) => {
   return (
     <>
+      <Head>
+        <title>Shows | Double Negative</title>
+        <meta
+          name="description"
+          content="Upcoming concerts curated by the Double Negative staff."
+        />
+        <meta property="og:title" content="Shows | Double Negative" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={`http://doublenegative.cc/shows`} />
+        <meta property="og:image" content="/public/og_image.png" />
+      </Head>
       <div className="container">
         <h1 className="page-title">Shows</h1>
         <FilterBar
